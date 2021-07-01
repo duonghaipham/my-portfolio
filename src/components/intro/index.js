d2eac683bb9267b79caa3296b842aeb6e3072730
= <phduongit2k1@gmail.com>
2021-07-01 22:49:48 +0700

    Add waving animation on welcome page

--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -11,6 +11,11 @@ const Intro = () => {
             <div id="image">
                 <img src={avt} alt={'Tyler'}/>
             </div>
+            <div id="wave-container">
+                <div className="wave wave1"></div>
+                <div className="wave wave2"></div>
+                <div className="wave wave3"></div>
+            </div>
         </section>
     );
 };
