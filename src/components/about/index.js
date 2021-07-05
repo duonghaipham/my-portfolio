e121646e238e7a19b73a0ac40ee9fbb3fb084d0e
= <phduongit2k1@gmail.com>
2021-07-05 14:34:04 +0700

    Complete the timeline skills

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,40 +1,56 @@
 import './style.scss';
 
 const About = () => {
+    const skills = [
+        {
+            'time': 'Nov 2020',
+            'title': 'Windows Form .NET',
+            'detail': 'I have self-studied to build an application run on .NET, known how to connect to SQL Server by Entity Framework'
+        },
+        {
+            'time': 'Mar 2021',
+            'title': 'HTML and CSS',
+            'detail': 'I have learn very basic HTML and CSS on freeCodeCamp.org and built some simple projects'
+        },
+        {
+            'time': 'Apr 2021',
+            'title': 'Android Java',
+            'detail': 'This is one of the most fascinated things I\'ve learnt, I developed lots of projects, know how to apply (both third-part\'s and my own) RESTful API by some related libraries (Retrofit, OkHttp, Picasso...)'
+        },
+        {
+            'time': 'May 2021',
+            'title': 'JavaScript, PHP and MySQL',
+            'detail': 'I seriously realized the important of JavaScript in a web app, used JQuery to make an AJAX request. I learnt basically about software architecture, especially MVC pattern, then I manipulated it into the current project'
+        },
+        {
+            'time': 'Jul 2021',
+            'title': 'ReactJS',
+            'detail': 'My portfolio is the first ever project that I build based on ReactJS'
+        }
+    ];
+
     return (
-      <section id="about">
-          <div id="timeline">
-              <div className="center-line"></div>
-              <div className="row row-1">
-                  <section>
-                      <div className="circle"></div>
-                      <div className="details">
-                          <h3>Title 1</h3>
-                          <h4>January 2021</h4>
-                      </div>
-                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                      <div className="bottom">
-                          <a href="#">Read more</a>
-                          <i>Test</i>
-                      </div>
-                  </section>
-              </div>
-              <div className="row row-2">
-                  <section>
-                      <div className="circle"></div>
-                      <div className="details">
-                          <h3>Title 1</h3>
-                          <h4>January 2021</h4>
-                      </div>
-                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                      <div className="bottom">
-                          <a href="#">Read more</a>
-                          <i>Test</i>
-                      </div>
-                  </section>
-              </div>
-          </div>
-      </section>
+        <section id="about">
+            <div id="timeline">
+                <h1>Skills as timeline</h1>
+                <div className="center-line" />
+                {
+                    skills.map((value, index) => {
+                        const side = (index % 2 === 0) ? 'left' : 'right';
+                        return (
+                            <div className={"row row-" + side}>
+                                <div className="content">
+                                    <div className="circle" />
+                                    <h3>{value['title']}</h3>
+                                    <p>{value['detail']}</p>
+                                </div>
+                                <h4>{value['time']}</h4>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </section>
     );
 };
 
